826e9bc2cadd55ed7d8db5e4ff22e593804ebfbb
Agwexy <aryaagun04@gmail.com>
2025-06-25 11:58:27 +0700

    Connect to ngrok backend and update env

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './pages/login';
+import Login from './pages/Login';
 import InventorySalesSystem from './pages/InventorySalesSystem';
 
 function App() {
